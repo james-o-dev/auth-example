49b099e9e0ead9226d8906c6de46353fed55bf48
James O <jameso@traffic.net.nz>
2023-12-21 14:08:43 +1300

    refactor(aws/lambda): Update the lambda function
    * Basic changes
    * Correct exports

---
+++ aws/lambda/auth-example-lambda/index.mjs
@@ -0,0 +1,52 @@
+
+/**
+ * Helper to build a Lambda response object.
+ *
+ * @param {number} statusCode
+ * @param {*} body
+ * @param {*} [options]
+ */
+const buildLambdaResponse = (statusCode, body, options = {}) => {
+  return {
+    statusCode,
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers,
+    },
+    body: JSON.stringify(body)
+  }
+}
+
+// index.js
+export const handler = async (event) => {
+  try {
+    // if (process.env.DEBUG) console.log('event :>> ', event)
+
+    // Get attributes of the request.
+    const reqBody = JSON.parse(event.body)
+    const reqMethod = event.httpMethod
+    const reqPath = event.path
+    const reqResourcePath = event.requestContext.resourcePath
+    const reqPathParameters = event.pathParameters
+    const reqQueryStringParameters = event.queryStringParameters
+
+    // Console log attributes.
+    console.log({
+      reqBody,
+      reqMethod,
+      reqPath,
+      reqPathParameters,
+      reqQueryStringParameters,
+      reqResourcePath,
+    })
+
+    if (reqPath === '/health' && reqMethod === 'GET') return buildLambdaResponse(200, 'I am healthy! ❤❤❤')
+
+    // API not found or implemented.
+    return buildLambdaResponse(404, 'API endpoint not found')
+
+  } catch (error) {
+    console.error('Error:', error)
+    return buildLambdaResponse(500, 'Internal Server Error')
+  }
+}
