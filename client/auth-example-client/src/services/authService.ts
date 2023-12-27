a445c216c17e2d9c5c32bbf3b866a5bffe791897
James O <jameso@traffic.net.nz>
2023-12-27 17:23:17 +1300

    feat(client): Fetch and display the API health

--- client/auth-example-client/src/services/authService.ts
+++ client/auth-example-client/src/services/authService.ts
@@ -2,9 +2,27 @@
 
 const API_BASE = '' // Add the API Gateway base URL here.
 
+/**
+ * Fetches the health status from the API endpoint.
+ */
+export const apiHealth = async () => {
+  try {
+    if (!API_BASE) throw new Error('API_BASE is not set.')
+
+    const response = await fetch(`${API_BASE}/health`)
+    const healthResponse = await response.json()
+    return healthResponse
+  } catch (error) {
+    return 'Not good ☹'
+  }
+}
+
+/**
+ * Fetch the API whether the user is authenticated.
+ */
 export const isAuthenticated = async () => {
   try {
-    if (!API_BASE) throw new Error('API_BASE is not set!')
+    if (!API_BASE) throw new Error('API_BASE is not set.')
 
     // Simulate an asynchronous operation, e.g., fetching user data from a server
     const response = await fetch(`${API_BASE}/auth`, {
