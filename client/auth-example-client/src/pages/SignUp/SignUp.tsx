8cc8c031cd507196d0da44d78d368130f00d8543
James O <jameso@traffic.net.nz>
2023-12-28 11:15:01 +1300

    feat(client): Updates sign-in page
    * Adds extra field to confirm password
    * Adds reset form button

--- client/auth-example-client/src/pages/SignUp/SignUp.tsx
+++ client/auth-example-client/src/pages/SignUp/SignUp.tsx
@@ -8,12 +8,36 @@ const SignUp: React.FC = () => {
   // State to hold form input values
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [validationMessages, setValidationMessages] = useState([] as string[])
   const [isSubmitting, setIsSubmitting] = useState(false)
   const navigate = useNavigate()
 
-  // Function to handle form submission
+  // Function to validate form input values before submission.
+  const validateForm = () => {
+    const newMessages: string[] = []
+
+    if (password !== confirmPassword) newMessages.push('Passwords do not match')
+
+    setValidationMessages(newMessages)
+
+    return newMessages.length === 0
+  }
+
+  /**
+   * Function to handle form submission.
+   *
+   * This function will validate the form input values before submitting.
+   * If the form is valid, it will make an API call to the backend to handle signup logic.
+   * If the API call is successful, it will display an alert and navigate to the protected page.
+   * If the API call fails, it will display an alert and reset the form.
+   *
+   * @param {React.FormEvent} event - The form submission event.
+   */
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
+
+    if (!validateForm()) return
 
     // You can make an API call or handle signup logic as per your application requirements
     // For simplicity, logging the input values to the console in this example
@@ -27,6 +51,17 @@ const SignUp: React.FC = () => {
     } finally {
       setIsSubmitting(false)
     }
+  }
+
+  /**
+   * Function to reset the form input values and validation messages.
+   */
+  const resetForm = () => {
+    setEmail('')
+    setPassword('')
+    setConfirmPassword('')
+    setValidationMessages([])
+    setIsSubmitting(false)
   }
 
   return (
@@ -54,7 +89,28 @@ const SignUp: React.FC = () => {
           />
         </label>
         <br />
-        <button type="submit">Sign Up</button>
+        <label>
+          Confirm password:
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        {validationMessages.length > 0 && (
+          <div>
+            <h3>Validation errors:</h3>
+            <ul>
+              {validationMessages.map((message, index) => (
+                <li key={index}>{message}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <button type="submit">SIGN UP</button>
+        <button type="button" onClick={resetForm}>reset</button>
         {isSubmitting && <span>Signing up...</span>}
       </form>
       <div>
