f99f5aa3533c63a71386bea448b4ca1844880614
James O <jameso@traffic.net.nz>
2023-12-28 23:27:44 +1300

    build(aws/lambda): Add script to remove unnecessary node_modules and package files
    * Add npm script

---
+++ aws/lambda/scripts/clean-lambda-node-modules.sh
@@ -0,0 +1,8 @@
+ #!/bin/bash
+
+rimraf ../auth-example-lambda/node_modules \
+  ../auth-example-lambda/package.json \
+  ../auth-example-lambda/package-lock.json \
+  ../auth-example-lambda/lib/node_modules \
+  ../auth-example-lambda/lib/package.json \
+  ../auth-example-lambda/lib/package-lock.json
