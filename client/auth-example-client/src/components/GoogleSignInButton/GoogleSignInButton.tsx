04de712c142d3117f8f5bbdf48c03704d76822e3
James O Dev <james.o.dev@gmail.com>
2024-01-06 10:56:47 +1300

    refactor(client): Update dark mode handling for Google SSO button

--- client/auth-example-client/src/components/GoogleSignInButton/GoogleSignInButton.tsx
+++ client/auth-example-client/src/components/GoogleSignInButton/GoogleSignInButton.tsx
@@ -3,10 +3,10 @@ import googleLogo from '../../assets/google.svg'
 
 const GoogleSignInButton = ({ onClick }: { onClick: VoidFunction }) => {
   return (
-    <button className='gsi-material-button w-full' onClick={onClick}>
+    <button className='gsi-material-button w-full dark:bg-neutral-500 dark:text-white' onClick={onClick}>
       <div className='gsi-material-button-state'></div>
       <div className='gsi-material-button-content-wrapper'>
-        <div className='gsi-material-button-icon'>
+        <div className='gsi-material-button-icon rounded-full dark:bg-white dark:p-1'>
           <img src={googleLogo} alt='Google' />
         </div>
         <span className='gsi-material-button-contents'>Sign in with Google</span>
