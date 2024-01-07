968bb063da2af5750b49b2a4d392b98cbb2cbab9
James O Dev <james.o.dev@gmail.com>
2024-01-08 11:32:30 +1300

    refactor(tests/client): Extra test for light/dark mode

--- tests/client-e2e/cypress/e2e/health.cy.js
+++ tests/client-e2e/cypress/e2e/health.cy.js
@@ -1,8 +1,18 @@
 describe('Check health', () => {
-  it('passes', () => {
+  beforeEach(() => {
     cy.visit('/')
+  })
 
+  it('passes', () => {
     cy.get('h2').should('contain', 'Hello World!')
     cy.get('p').should('contain', 'API health: I am healthy! ❤❤❤')
+  })
+
+  it('toggle light/dark mode', () => {
+    cy.get('header button img[alt="Dark mode"').click()
+    cy.get('div.dark').should('exist')
+
+    cy.get('header button img[alt="Light mode"').click()
+    cy.get('div.dark').should('not.exist')
   })
 })
